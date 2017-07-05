e0804697eeed4eeb4ecbeee636aa451673ab35d6
linnilin <antje.rogotzki@arquest.ch>
2017-07-05 13:57:26 +0000

    second

--- src/main/java/org/eclipse/che/examples/GreetingController.java
+++ src/main/java/org/eclipse/che/examples/GreetingController.java
@@ -16,7 +16,7 @@ public class GreetingController implements Controller
       String result = "";
       if (userName != null)
       {
-        result = "Hello, " + userName + "!";
+        result = "Hello, yyy " + userName + "!";
       }
 
       ModelAndView view = new ModelAndView("hello_view");
